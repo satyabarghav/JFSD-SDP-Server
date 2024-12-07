c600d0313f41a5ff857e36ceb5fd6aa2a81f6a0b
Appari Satya Barghav <satyabarghav1@gmail.com>
2024-12-08 02:23:01 +0530

    Added Frontend URL to WebConfig and resolved CORS

--- src/main/java/com/klef/jfsd/sdp/config/WebConfig.java
+++ src/main/java/com/klef/jfsd/sdp/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://jfsd-sdp-server-production.up.railway.app")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
